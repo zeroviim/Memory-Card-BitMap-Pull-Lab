3d30abd59077c8ca8e3355a9378f81e7f95b3af0
zeroviim <43391837+zeroviim@users.noreply.github.com>
2020-01-03 13:40:09 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Testing_bitshifting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_color0 = new System.Windows.Forms.Label();
+            this.lbl_Color1 = new System.Windows.Forms.Label();
+            this.lbl_Color3 = new System.Windows.Forms.Label();
+            this.lbl_Color2 = new System.Windows.Forms.Label();
+            this.lbl_Color7 = new System.Windows.Forms.Label();
+            this.lbl_Color6 = new System.Windows.Forms.Label();
+            this.lbl_Color5 = new System.Windows.Forms.Label();
+            this.lbl_Color4 = new System.Windows.Forms.Label();
+            this.lbl_Color15 = new System.Windows.Forms.Label();
+            this.lbl_Color14 = new System.Windows.Forms.Label();
+            this.lbl_Color13 = new System.Windows.Forms.Label();
+            this.lbl_Color12 = new System.Windows.Forms.Label();
+            this.lbl_Color11 = new System.Windows.Forms.Label();
+            this.lbl_Color10 = new System.Windows.Forms.Label();
+            this.lbl_Color9 = new System.Windows.Forms.Label();
+            this.lbl_Color8 = new System.Windows.Forms.Label();
+            this.pcbx_Frame1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbx_Frame1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_color0
+            // 
+            this.lbl_color0.AutoSize = true;
+            this.lbl_color0.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_color0.Location = new System.Drawing.Point(31, 25);
+            this.lbl_color0.Name = "lbl_color0";
+            this.lbl_color0.Size = new System.Drawing.Size(33, 15);
+            this.lbl_color0.TabIndex = 1;
+            this.lbl_color0.Text = "        ";
+            // 
+            // lbl_Color1
+            // 
+            this.lbl_Color1.AutoSize = true;
+            this.lbl_Color1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color1.Location = new System.Drawing.Point(31, 54);
+            this.lbl_Color1.Name = "lbl_Color1";
+            this.lbl_Color1.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color1.TabIndex = 2;
+            this.lbl_Color1.Text = "        ";
+            // 
+            // lbl_Color3
+            // 
+            this.lbl_Color3.AutoSize = true;
+            this.lbl_Color3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color3.Location = new System.Drawing.Point(31, 113);
+            this.lbl_Color3.Name = "lbl_Color3";
+            this.lbl_Color3.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color3.TabIndex = 4;
+            this.lbl_Color3.Text = "        ";
+            // 
+            // lbl_Color2
+            // 
+            this.lbl_Color2.AutoSize = true;
+            this.lbl_Color2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color2.Location = new System.Drawing.Point(31, 84);
+            this.lbl_Color2.Name = "lbl_Color2";
+            this.lbl_Color2.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color2.TabIndex = 3;
+            this.lbl_Color2.Text = "        ";
+            // 
+            // lbl_Color7
+            // 
+            this.lbl_Color7.AutoSize = true;
+            this.lbl_Color7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color7.Location = new System.Drawing.Point(31, 225);
+            this.lbl_Color7.Name = "lbl_Color7";
+            this.lbl_Color7.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color7.TabIndex = 8;
+            this.lbl_Color7.Text = "        ";
+            // 
+            // lbl_Color6
+            // 
+            this.lbl_Color6.AutoSize = true;
+            this.lbl_Color6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color6.Location = new System.Drawing.Point(31, 196);
+            this.lbl_Color6.Name = "lbl_Color6";
+            this.lbl_Color6.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color6.TabIndex = 7;
+            this.lbl_Color6.Text = "        ";
+            // 
+            // lbl_Color5
+            // 
+            this.lbl_Color5.AutoSize = true;
+            this.lbl_Color5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color5.Location = new System.Drawing.Point(31, 166);
+            this.lbl_Color5.Name = "lbl_Color5";
+            this.lbl_Color5.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color5.TabIndex = 6;
+            this.lbl_Color5.Text = "        ";
+            // 
+            // lbl_Color4
+            // 
+            this.lbl_Color4.AutoSize = true;
+            this.lbl_Color4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color4.Location = new System.Drawing.Point(31, 137);
+            this.lbl_Color4.Name = "lbl_Color4";
+            this.lbl_Color4.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color4.TabIndex = 5;
+            this.lbl_Color4.Text = "        ";
+            // 
+            // lbl_Color15
+            // 
+            this.lbl_Color15.AutoSize = true;
+            this.lbl_Color15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color15.Location = new System.Drawing.Point(82, 225);
+            this.lbl_Color15.Name = "lbl_Color15";
+            this.lbl_Color15.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color15.TabIndex = 16;
+            this.lbl_Color15.Text = "        ";
+            // 
+            // lbl_Color14
+            // 
+            this.lbl_Color14.AutoSize = true;
+            this.lbl_Color14.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color14.Location = new System.Drawing.Point(82, 196);
+            this.lbl_Color14.Name = "lbl_Color14";
+            this.lbl_Color14.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color14.TabIndex = 15;
+            this.lbl_Color14.Text = "        ";
+            // 
+            // lbl_Color13
+            // 
+            this.lbl_Color13.AutoSize = true;
+            this.lbl_Color13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color13.Location = new System.Drawing.Point(82, 166);
+            this.lbl_Color13.Name = "lbl_Color13";
+            this.lbl_Color13.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color13.TabIndex = 14;
+            this.lbl_Color13.Text = "        ";
+            // 
+            // lbl_Color12
+            // 
+            this.lbl_Color12.AutoSize = true;
+            this.lbl_Color12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color12.Location = new System.Drawing.Point(82, 137);
+            this.lbl_Color12.Name = "lbl_Color12";
+            this.lbl_Color12.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color12.TabIndex = 13;
+            this.lbl_Color12.Text = "        ";
+            // 
+            // lbl_Color11
+            // 
+            this.lbl_Color11.AutoSize = true;
+            this.lbl_Color11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color11.Location = new System.Drawing.Point(82, 113);
+            this.lbl_Color11.Name = "lbl_Color11";
+            this.lbl_Color11.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color11.TabIndex = 12;
+            this.lbl_Color11.Text = "        ";
+            // 
+            // lbl_Color10
+            // 
+            this.lbl_Color10.AutoSize = true;
+            this.lbl_Color10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color10.Location = new System.Drawing.Point(82, 84);
+            this.lbl_Color10.Name = "lbl_Color10";
+            this.lbl_Color10.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color10.TabIndex = 11;
+            this.lbl_Color10.Text = "        ";
+            // 
+            // lbl_Color9
+            // 
+            this.lbl_Color9.AutoSize = true;
+            this.lbl_Color9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color9.Location = new System.Drawing.Point(82, 54);
+            this.lbl_Color9.Name = "lbl_Color9";
+            this.lbl_Color9.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color9.TabIndex = 10;
+            this.lbl_Color9.Text = "        ";
+            // 
+            // lbl_Color8
+            // 
+            this.lbl_Color8.AutoSize = true;
+            this.lbl_Color8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Color8.Location = new System.Drawing.Point(82, 25);
+            this.lbl_Color8.Name = "lbl_Color8";
+            this.lbl_Color8.Size = new System.Drawing.Size(33, 15);
+            this.lbl_Color8.TabIndex = 9;
+            this.lbl_Color8.Text = "        ";
+            // 
+            // pcbx_Frame1
+            // 
+            this.pcbx_Frame1.Location = new System.Drawing.Point(144, 25);
+            this.pcbx_Frame1.Name = "pcbx_Frame1";
+            this.pcbx_Frame1.Size = new System.Drawing.Size(115, 103);
+            this.pcbx_Frame1.TabIndex = 17;
+            this.pcbx_Frame1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 274);
+            this.Controls.Add(this.pcbx_Frame1);
+            this.Controls.Add(this.lbl_Color15);
+            this.Controls.Add(this.lbl_Color14);
+            this.Controls.Add(this.lbl_Color13);
+            this.Controls.Add(this.lbl_Color12);
+            this.Controls.Add(this.lbl_Color11);
+            this.Controls.Add(this.lbl_Color10);
+            this.Controls.Add(this.lbl_Color9);
+            this.Controls.Add(this.lbl_Color8);
+            this.Controls.Add(this.lbl_Color7);
+            this.Controls.Add(this.lbl_Color6);
+            this.Controls.Add(this.lbl_Color5);
+            this.Controls.Add(this.lbl_Color4);
+            this.Controls.Add(this.lbl_Color3);
+            this.Controls.Add(this.lbl_Color2);
+            this.Controls.Add(this.lbl_Color1);
+            this.Controls.Add(this.lbl_color0);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbx_Frame1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_color0;
+        private System.Windows.Forms.Label lbl_Color1;
+        private System.Windows.Forms.Label lbl_Color3;
+        private System.Windows.Forms.Label lbl_Color2;
+        private System.Windows.Forms.Label lbl_Color7;
+        private System.Windows.Forms.Label lbl_Color6;
+        private System.Windows.Forms.Label lbl_Color5;
+        private System.Windows.Forms.Label lbl_Color4;
+        private System.Windows.Forms.Label lbl_Color15;
+        private System.Windows.Forms.Label lbl_Color14;
+        private System.Windows.Forms.Label lbl_Color13;
+        private System.Windows.Forms.Label lbl_Color12;
+        private System.Windows.Forms.Label lbl_Color11;
+        private System.Windows.Forms.Label lbl_Color10;
+        private System.Windows.Forms.Label lbl_Color9;
+        private System.Windows.Forms.Label lbl_Color8;
+        private System.Windows.Forms.PictureBox pcbx_Frame1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_bitshifting
+{
+    public partial class Form1 : Form
+    {
+        public byte[] paletteBytes;
+        public byte[] bitmapBytes;
+        public Color[] colorbytes;
+        public string pathToMCR = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bitmapBytes = new byte[0x80];
+            BinaryReader MCR = new BinaryReader(File.Open(pathToMCR, FileMode.Open));
+            MCR.BaseStream.Position = 0x2080;
+            bitmapBytes = MCR.ReadBytes(0x80);
+            colorbytes = new Color[16];
+            /*paletteBytes = new byte[32] { 0x00, 0x00, 0x3F, 0x73, 0xBD, 0xDE, 0x7B, 0x52,
+                                          0x19, 0x46, 0xD7, 0x39, 0x5F, 0x02, 0x54, 0x25,
+                                          0xAB, 0x00, 0xFF, 0x7F, 0x9F, 0x73, 0x5F, 0x67,
+                                          0x1F, 0x5F, 0x45, 0x7D, 0x36, 0x7F, 0xC4, 0x7A }; //big endian*/
+            paletteBytes = new byte[32] { 0x00, 0x00, 0x73, 0x3F, 0xDE, 0xBD, 0x52, 0x7B, 
+                                          0x46, 0x19, 0x39, 0xD7, 0x02, 0x5F, 0x25, 0x54,
+                                          0x00, 0xAB, 0x7F, 0xFF, 0x73, 0x9F, 0x67, 0x5F,
+                                          0x5F, 0x1F, 0x7D, 0x45, 0x7F, 0x36, 0x7A, 0xC4 }; //little endian straight
+            //Array.Reverse(paletteBytes, 0, 32);
+            GeneratePaletteArray(paletteBytes);
+            lbl_color0.BackColor = colorbytes[0];
+            lbl_Color1.BackColor = colorbytes[1];
+            lbl_Color2.BackColor = colorbytes[2];
+            lbl_Color3.BackColor = colorbytes[3];
+            lbl_Color4.BackColor = colorbytes[4];
+            lbl_Color5.BackColor = colorbytes[5];
+            lbl_Color6.BackColor = colorbytes[6];
+            lbl_Color7.BackColor = colorbytes[7];
+            lbl_Color8.BackColor = colorbytes[8];
+            lbl_Color9.BackColor = colorbytes[9];
+            lbl_Color10.BackColor = colorbytes[10];
+            lbl_Color11.BackColor = colorbytes[11];
+            lbl_Color12.BackColor = colorbytes[12];
+            lbl_Color13.BackColor = colorbytes[13];
+            lbl_Color14.BackColor = colorbytes[14];
+            lbl_Color15.BackColor = colorbytes[15];
+            GenerateBitmap();
+        }
+
+        private void GenerateBitmap()
+        {
+            Bitmap bitmapFrameOutput = new Bitmap(16, 16);
+            int bitmapCount = 0;
+            for (int y = 0; y < 16; y++)
+            {
+                for (int x = 0; x < 16; x+= 2)
+                {
+                    byte leftNybble = (byte)(bitmapBytes[bitmapCount] >> 4);
+                    byte rightNybble = (byte)(bitmapBytes[bitmapCount] << 4);
+                    rightNybble = (byte)(rightNybble >> 4);
+                    bitmapFrameOutput.SetPixel(x, y, colorbytes[rightNybble]);
+                    bitmapFrameOutput.SetPixel(x+1, y, colorbytes[leftNybble]);
+                    bitmapCount++;
+                }
+            }
+            pcbx_Frame1.Image = bitmapFrameOutput;
+            pcbx_Frame1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void GeneratePaletteArray(byte[] paletteBytes)
+        {
+            byte red5Bit, green5Bit, blue5Bit = 0;
+            int red8Bit, green8Bit, blue8Bit = 0;
+            byte tempByte1, tempByte2 = 0;
+            int colorByteCounter = 0;
+            for (int i = 0; i < paletteBytes.Length; i += 2)
+            {
+                byte byte1 = paletteBytes[i];
+                byte byte2 = paletteBytes[i+1];
+                red5Bit = 0;
+                green5Bit = 0;
+                blue5Bit = 0;
+                red8Bit = 0;
+                green8Bit = 0;
+                blue8Bit = 0;
+                tempByte1 = 0;
+                tempByte2 = 0;
+                if (colorByteCounter == 1)
+                {
+                    tempByte1 = 0;
+                }
+                tempByte1 = (byte)(byte1 << 1);
+                tempByte1 = (byte)(tempByte1 >> 3);
+                red5Bit = tempByte1;
+
+                tempByte1 = (byte)(byte1 << 6);
+                tempByte1 = (byte)(tempByte1 >> 3);
+                tempByte2 = (byte)(byte2 >> 5);
+                green5Bit = (byte)(tempByte1 | tempByte2);
+
+                tempByte1 = (byte)(byte2 << 3);
+                tempByte1 = (byte)(tempByte1 >> 3);
+                blue5Bit = tempByte1;
+
+                /*
+                red5Bit = (byte)(byte1 >> 3);
+
+                tempByte1 = (byte)(byte1 << 5);
+                tempByte1 = (byte)(tempByte1 >> 3);
+                tempByte2 = (byte)(byte2 >> 6);
+                green5Bit = (byte)(tempByte1 | tempByte2);
+
+                tempByte1 = (byte)(byte2 << 2);
+                tempByte1 = (byte)(tempByte1 >> 3);
+                blue5Bit = tempByte1;*/
+
+                red8Bit = (red5Bit * 255) / 31;
+                green8Bit = (green5Bit * 255) / 31;
+                blue8Bit = (blue5Bit * 255) / 31;
+
+                Color color = new Color();
+                //color = Color.FromArgb(blue8Bit, red8Bit, green8Bit);
+                color = Color.FromArgb(blue8Bit, green8Bit, red8Bit);
+                //color = Color.FromArgb(red8Bit, green8Bit, blue8Bit);
+                //color = Color.FromArgb(red8Bit, blue8Bit, green8Bit);
+                colorbytes[colorByteCounter] = color;
+                colorByteCounter++;
+                /*MessageBox.Show(@"Original Bytes:" + paletteBytes[i].ToString("X2") + " | " + paletteBytes[i + 1].ToString("X2") + "\r\n" +
+                                "Original Byte Binary Readout: " + Convert.ToString(Math.Abs(paletteBytes[i]), 2) + " | " + Convert.ToString(Math.Abs(paletteBytes[i+1]), 2) + "\r\n" +
+                                "Binary Conversions: " + Convert.ToString(Math.Abs(red5Bit), 2) + " | " + Convert.ToString(Math.Abs(green5Bit), 2) + " | " + Convert.ToString(Math.Abs(blue5Bit), 2) + "\r\n" +
+                                "Converted: " + red8Bit.ToString() + " | " + green8Bit.ToString() + " | " + blue8Bit.ToString());*/
+            }
+        }
+    }
+}
